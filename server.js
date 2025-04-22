75393c061f2affe93efdb0fe14fafdb1a7f5d2f9
Naviyanka <naviyanka@outlook.com>
2025-04-22 19:29:42 +0530

    Update

--- server.js
+++ server.js
@@ -3,13 +3,32 @@ const { parse } = require('url');
 const next = require('next');
 const path = require('path');
 
+// Set memory limit
+process.env.NODE_OPTIONS = process.env.NODE_OPTIONS || '--max-old-space-size=4096';
+
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 
 const app = next({ 
   dev,
-  dir: path.join(__dirname, 'app')
+  dir: path.join(__dirname, 'app'),
+  conf: {
+    webpack: (config, { isServer }) => {
+      if (!isServer) {
+        config.optimization.splitChunks.cacheGroups = {
+          ...config.optimization.splitChunks.cacheGroups,
+          'react-vendor': {
+            test: /[\\/]node_modules[\\/](react|react-dom)[\\/]/,
+            name: 'react-vendor',
+            chunks: 'all',
+          },
+        };
+      }
+      return config;
+    },
+  }
 });
+
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
